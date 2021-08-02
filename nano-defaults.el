cc0a7fba47c96d72689d5ae6b26948023f369f40
zainab-ali <zainab@kebab-ca.se>
2021-08-03 00:14:11 +0100

    Remove osx copy

--- nano-defaults.el
+++ nano-defaults.el
@@ -116,16 +116,16 @@
       mac-use-title-bar nil)
 
 ;; Make sure clipboard works properly in tty mode on OSX
-(defun copy-from-osx ()
-  (shell-command-to-string "pbpaste"))
-(defun paste-to-osx (text &optional push)
-  (let ((process-connection-type nil))
-    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-      (process-send-string proc text)
-      (process-send-eof proc))))
-(if (not (display-graphic-p))
-    (progn (setq interprogram-cut-function 'paste-to-osx)
-           (setq interprogram-paste-function 'copy-from-osx)))
+;; (defun copy-from-osx ()
+;;   (shell-command-to-string "pbpaste"))
+;; (defun paste-to-osx (text &optional push)
+;;   (let ((process-connection-type nil))
+;;     (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+;;       (process-send-string proc text)
+;;       (process-send-eof proc))))
+;; (if (not (display-graphic-p))
+;;     (progn (setq interprogram-cut-function 'paste-to-osx)
+;;            (setq interprogram-paste-function 'copy-from-osx)))
 
 ;; y/n for  answering yes/no questions
 (fset 'yes-or-no-p 'y-or-n-p)
