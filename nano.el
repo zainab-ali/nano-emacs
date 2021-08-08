4298ae3e20209a27c4012dab4b62154a5f64185c
zainab-ali <zainab@kebab-ca.se>
2021-08-08 11:58:43 +0100

    Remove most options

--- nano.el
+++ nano.el
@@ -18,71 +18,71 @@
 (package-initialize)
 
 ;; Path to nano emacs modules (mandatory)
-(add-to-list 'load-path "/Users/rougier/Documents/GitHub/nano-emacs")
+;; (add-to-list 'load-path "/Users/rougier/Documents/GitHub/nano-emacs")
 (add-to-list 'load-path ".")
 
 ;; Window layout (optional)
 (require 'nano-layout)
 
 ;; Theming Command line options (this will cancel warning messages)
-(add-to-list 'command-switch-alist '("-dark"   . (lambda (args))))
-(add-to-list 'command-switch-alist '("-light"  . (lambda (args))))
-(add-to-list 'command-switch-alist '("-default"  . (lambda (args))))
+;; (add-to-list 'command-switch-alist '("-dark"   . (lambda (args))))
+;; (add-to-list 'command-switch-alist '("-light"  . (lambda (args))))
+;; (add-to-list 'command-switch-alist '("-default"  . (lambda (args))))
 
-(cond
- ((member "-default" command-line-args) t)
- ((member "-dark" command-line-args) (require 'nano-theme-dark))
- (t (require 'nano-theme-light)))
+;; (cond
+;;  ((member "-default" command-line-args) t)
+;;  ((member "-dark" command-line-args) (require 'nano-theme-dark))
+;;  (t (require 'nano-theme-light)))
 
 ;; Customize support for 'emacs -q' (Optional)
 ;; You can enable customizations by creating the nano-custom.el file
 ;; with e.g. `touch nano-custom.el` in the folder containing this file.
-(let* ((this-file  (or load-file-name (buffer-file-name)))
-       (this-dir  (file-name-directory this-file))
-       (custom-path  (concat this-dir "nano-custom.el")))
-  (when (and (eq nil user-init-file)
-             (eq nil custom-file)
-             (file-exists-p custom-path))
-    (setq user-init-file this-file)
-    (setq custom-file custom-path)
-    (load custom-file)))
+;; (let* ((this-file (or load-file-name (buffer-file-name)))
+;;        (this-dir (file-name-directory this-file))
+;;        (custom-path (concat this-dir "nano-custom.el")))
+;;   (when (and (eq nil user-init-file)
+;; 	     (eq nil custom-file)
+;; 	     (file-exists-p custom-path))
+;;     (setq user-init-file this-file)
+;;     (setq custom-file custom-path)
+;;     (load custom-file)))
 
 ;; Theme
-(require 'nano-faces)
-(nano-faces)
+;; (require 'nano-faces)
+;; (nano-faces)
 
-(require 'nano-theme)
-(nano-theme)
+;; (require 'nano-theme)
+;; (nano-theme)
 
 ;; Nano default settings (optional)
 (require 'nano-defaults)
 
 ;; Nano session saving (optional)
-(require 'nano-session)
+;; (require 'nano-session)
 
-;; Nano header & mode lines (optional)
 (require 'nano-modeline)
+;; Nano header & mode lines (optional)
 
 ;; Nano key bindings modification (optional)
-(require 'nano-bindings)
+;; (require 'nano-bindings)
 
 ;; Nano counsel configuration (optional)
 ;; Needs "counsel" package to be installed (M-x: package-install)
 ;; (require 'nano-counsel)
 
 ;; Welcome message (optional)
-(let ((inhibit-message t))
-  (message "Welcome to GNU Emacs / N Λ N O edition")
-  (message (format "Initialization time: %s" (emacs-init-time))))
+;; (let ((inhibit-message t))
+;;   (message "Welcome to GNU Emacs / N Λ N O edition")
+;;   (message (format "Initialization time: %s" (emacs-init-time))))
 
 ;; Splash (optional)
-(add-to-list 'command-switch-alist '("-no-splash" . (lambda (args))))
-(unless (member "-no-splash" command-line-args)
-  (require 'nano-splash))
+;; (add-to-list 'command-switch-alist '("-no-splash" . (lambda (args))))
+;; (unless (member "-no-splash" command-line-args)
+;;   (require 'nano-splash))
 
 ;; Help (optional)
-(add-to-list 'command-switch-alist '("-no-help" . (lambda (args))))
-(unless (member "-no-help" command-line-args)
-  (require 'nano-help))
+;; (add-to-list 'command-switch-alist '("-no-help" . (lambda (args))))
+;; (unless (member "-no-help" command-line-args)
+;;   (require 'nano-help))
 
 (provide 'nano)
